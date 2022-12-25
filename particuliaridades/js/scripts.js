4ea3b59ffce555b89f9f669e72055c644a691196
erickpetterson <epettersondossantos@gmail.com>
2022-12-25 15:28:38 -0400

    segundo commit

---
+++ particuliaridades/js/scripts.js
@@ -0,0 +1,22 @@
+console.log("Primeiro");
+
+console.log("ultimo");
+
+//declara uma variavel
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// este e um comentario
+
+/*
+este 
+e 
+um
+comentario
+de 
+multiplas
+linha
+*/
