8776282946d0967ef02095ee9afe134a22c72617
erickpetterson <epettersondossantos@gmail.com>
2022-12-25 15:44:33 -0400

    segundo commit

---
+++ variaveis/js/scripts.js
@@ -0,0 +1,43 @@
+var teste = 1;
+
+nome = "joao";
+
+console.log(teste);
+
+teste = 'erick';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'teste';
+var _nome = 'teste2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'teste5';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'erick'
+
+console.log(meuPrimeiroNome); //canelcase
+
+var meusobrenome = 'petterson';
+
+console.log(meusobrenome);
+
+let testando =1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'erick';
+
+console.log(meuNome);
