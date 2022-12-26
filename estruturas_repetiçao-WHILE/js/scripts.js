db4a4ee779842ed0b95827114219bfd19c4f6c8d
erickpetterson <epettersondossantos@gmail.com>
2022-12-26 13:35:04 -0400

    terceiro Commit

---
+++ estruturas_repetiçao-WHILE/js/scripts.js
@@ -0,0 +1,26 @@
+var x = 0;
+
+while (x < 5) {
+
+    console.log("testando repetiçao " + x);
+
+    x++;
+
+};
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while (y <= 3) {
+    console.log(arr[y]);
+
+    y++;
+};
+
+var palavra = 'bob';
+var i = 0;
+while (i <= 3){
+    console.log(palavra[i]);
+
+    i += 1;
+};
